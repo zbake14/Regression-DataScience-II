c171a1fa2dd006ee7f3257a8cee341531450917f
zbake14 <zbake14@gmail.com>
2019-02-12 23:40:49 -0500

    small bug fixes and denotation with summary which lin regs are throwing NANs

--- Scala/src/main/scala/example/LinRegression.scala
+++ Scala/src/main/scala/example/LinRegression.scala
@@ -1,23 +1,24 @@
-package data
+package regression
 import scalation.linalgebra._
 import scalation.analytics._
 import scalation.stat.StatVector.corr
 import scalation.analytics.classifier._
 import scalation.util.{getFromURL_File, time}
+import data._
 
-object LinRegressAutoMpg extends App
+object LinRegress extends App
 {
 
 
-// val qrg = new Regression(ExampleAutoMPG.ox,ExampleAutoMPG.y)
-// qrg.train().eval()
-// println("Auto MPG:")
-// println(qrg.report)
+val qrg = new Regression(VectorD.one (ExampleAutoMPG.xy.dim1) +^: ExampleAutoMPG.x,ExampleAutoMPG.y)
+qrg.train().eval()
+println("Auto MPG:")
+println(qrg.report)
 
 val qrgBike = new Regression(BikeSharing.ox, BikeSharing.y)
 qrgBike.train().eval()
 println("Bike:")
-println(qrgBike.report)
+println(qrgBike.summary)
 
 val qrgComputer = new Regression(ComputerHardware.ox,ComputerHardware.y)
 qrgComputer.train().eval()
@@ -34,7 +35,7 @@ println(qrgElectricGrid.report)
 val qrgEnergyEff = new Regression(EnergyEff.ox,EnergyEff.y)
 qrgEnergyEff.train().eval()
 println("Energy Eff:")
-println(qrgEnergyEff.report)
+println(qrgEnergyEff.summary)
 
 
 val qrgForestFires = new Regression(ForestFires.ox,ForestFires.y)
@@ -45,7 +46,7 @@ println(qrgForestFires.report)
 val qrgNaval = new Regression(Naval.ox,Naval.y)
 qrgNaval.train().eval()
 println("Naval:")
-println(qrgNaval.report)
+println(qrgNaval.summary)
 
 
 val qrgoptical = new Regression(optical.ox,optical.y)
@@ -58,10 +59,10 @@ qrgProteinTertiary.train().eval()
 println("ProteinTertiary:")
 println(qrgProteinTertiary.report)
 
-// val qrgWineQuality = new Regression(WineQuality.ox,WineQuality.y)
-// qrgWineQuality.train().eval()
-// println("WineQuality:")
-// println(qrgWineQuality.report)
+val qrgWineQuality = new Regression(WineQuality.ox,WineQuality.y)
+qrgWineQuality.train().eval()
+println("WineQuality:")
+println(qrgWineQuality.report)
 
 
 
