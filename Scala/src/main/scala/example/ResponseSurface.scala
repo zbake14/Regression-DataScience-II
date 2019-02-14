ffddf5eb3d56a9e3779276bd84345905fbad7afe
Omkar Mukund Acharya <omkar.acharya@uga.edu>
2019-02-14 00:02:25 -0500

    ResponseSurface for optical dataset along with gitignore file

--- Scala/src/main/scala/example/ResponseSurface.scala
+++ Scala/src/main/scala/example/ResponseSurface.scala
@@ -13,20 +13,40 @@ object ResponseSurface extends App
 	val autoY = auto.y
 
 
-	val rsrg = new ResponseSurface(autoX,autoY)
-	rsrg.train().eval()
-	println("Auto MPG:")
-	println(rsrg.report)
+	// val rs = new ResponseSurface(autoX,autoY)
+	// rs.train().eval()
+	// println("Auto MPG:")
+	// println(rs.report)
 
-	val rsrgBike = new ResponseSurface(BikeSharing.ox,BikeSharing.y)
-	rsrgBike.train().eval()
-	println("Bike:")
-	println(rsrgBike.report)
+	// val lrgBike = new LassoRegression(BikeSharing.ox/*.selectCols(Array(1,20,21,22,23,24,25,26,27,28))*/, BikeSharing.y)
+	// lrgBike.train().eval()
+	// println("Bike:")
+	// println(lrgBike.report)
 
-	val rsrgComputer = new ResponseSurface(ComputerHardware.ox,ComputerHardware.y)
-	rsrgComputer.train().eval()
-	println("Computer:")
-	println(rsrgComputer.report)
+	// val lrgComputer = new LassoRegression(ComputerHardware.ox,ComputerHardware.y)
+	// lrgComputer.train().eval()
+	// println("Computer:")
+	// println(lrgComputer.report)
+
+	// val rsElectricGrid = new LassoRegression(ElectricalGrid.ox,ElectricalGrid.y)
+	// rsElectricGrid.train().eval()
+	// println("Electrical Grid:")
+	// println(rsElectricGrid.report)
+
+	// val rsEnergyEff = new LassoRegression(EnergyEff.ox,EnergyEff.y)
+	// rsEnergyEff.train().eval()
+	// println("Energy Eff:")
+	// println(rsEnergyEff.report)
+
+	// val rsForestFires = new LassoRegression(ForestFires.ox,ForestFires.y)
+	// rsForestFires.train().eval()
+	// println("ForestFires:")
+	// println(rsForestFires.report)
+
+	val rsOptical = new LassoRegression(optical.ox,optical.y)
+	rsOptical.train().eval()
+	println("Optical:")
+	println(rsOptical.report)
 
 
 }
