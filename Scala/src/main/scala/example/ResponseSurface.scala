88de300b1a8098848572374ebc6f58f20b743e33
Omkar Mukund Acharya <omkar.acharya@uga.edu>
2019-02-13 23:00:22 -0500

    Testing gitignore

--- Scala/src/main/scala/example/ResponseSurface.scala
+++ Scala/src/main/scala/example/ResponseSurface.scala
@@ -13,15 +13,20 @@ object ResponseSurface extends App
 	val autoY = auto.y
 
 
-	val rsrg = new QuadRegression(autoX,autoY)
+	val rsrg = new ResponseSurface(autoX,autoY)
 	rsrg.train().eval()
 	println("Auto MPG:")
 	println(rsrg.report)
 
- 	val rsrgBike = new Regression(BikeSharing.ox, BikeSharing.y)
- 	rsrgBike.train().eval()
- 	println("Bike:")
- 	println(rsrgBike.report)
+	val rsrgBike = new ResponseSurface(BikeSharing.ox,BikeSharing.y)
+	rsrgBike.train().eval()
+	println("Bike:")
+	println(rsrgBike.report)
+
+	val rsrgComputer = new ResponseSurface(ComputerHardware.ox,ComputerHardware.y)
+	rsrgComputer.train().eval()
+	println("Computer:")
+	println(rsrgComputer.report)
 
 
 }
